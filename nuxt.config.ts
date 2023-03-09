fdf7ab87008b5af6238101ba0f17a01df92dd594
beachislove <asperatuz@yandex.ru>
2023-03-10 00:17:51 +0600

    feat: add to netflify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,21 @@
 export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss'
-  ]
+  ],
+  app: {
+    head: {
+      title: 'Nuxt test market',
+      meta: [
+        {name: 'description', content: 'Test nuxt app'},
+
+      ],
+      link: [
+        {rel: 'stylesheet', href:'https://fonts.googleapis.com/css2?family=Material+Icons'}
+      ]
+    },
+    pageTransition: { name: 'page', mode: 'out-in' }
+  },
+  runtimeConfig:{
+    randomKey: process.env.TESTOVUY_API_KEY 
+  }
 })
